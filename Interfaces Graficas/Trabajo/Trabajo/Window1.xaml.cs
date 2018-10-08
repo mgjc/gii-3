8f3c7204d6a56fdae38d578a2f5437a41d92c4ac
Juan Carlos Martín García <jcmg@usal.es>
2018-10-08 14:56:11 +0200

    Optimizado
    codigo ligeramente optimizado para que no haya tanto, agrupacion en metodos para ocultamiento de textBox

--- Interfaces Graficas/Trabajo/Trabajo/Window1.xaml.cs
+++ Interfaces Graficas/Trabajo/Trabajo/Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Trabajo
 {
@@ -19,6 +20,9 @@ namespace Trabajo
     /// </summary>
     public partial class Window1 : Window
     {
+        int funcionSeleccionada = 0;
+        bool nomb_func = false, a=false, b=false, c=false;
+
         public Window1()
         {
             InitializeComponent();
@@ -31,45 +35,16 @@ namespace Trabajo
                 VarA1.Visibility = Visibility.Visible;
                 VarB1.Visibility = Visibility.Visible;
                 Nomb1.Visibility = Visibility.Visible;
-
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
-                this.VarA2.Text = "a";
-                this.VarB2.Text = "b";
-                this.VarA3.Text = "a";
-                this.VarB3.Text = "b";
-                this.VarA4.Text = "a";
-                this.VarB4.Text = "b";
-                this.VarA5.Text = "a";
-                this.VarB5.Text = "b";
-                this.VarC5.Text = "c";
-                this.VarA6.Text = "a";
-                this.VarB6.Text = "b";
-                this.Nomb2.Text = "Nombre";
-                this.Nomb3.Text = "Nombre";
-                this.Nomb4.Text = "Nombre";
-                this.Nomb5.Text = "Nombre";
-                this.Nomb6.Text = "Nombre";
+                funcionSeleccionada = 1;
+                OcultaFunc2();
+                OcultaFunc3();
+                OcultaFunc4();
+                OcultaFunc5();
+                OcultaFunc6();
             }
             else
             {
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
+                OcultaFunc1();
             }
         }
 
@@ -80,47 +55,16 @@ namespace Trabajo
                 VarA2.Visibility = Visibility.Visible;
                 VarB2.Visibility = Visibility.Visible;
                 Nomb2.Visibility = Visibility.Visible;
-
-
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
-                this.VarA1.Text = "a";
-                this.VarB1.Text = "b";
-                this.VarA3.Text = "a";
-                this.VarB3.Text = "b";
-                this.VarA4.Text = "a";
-                this.VarB4.Text = "b";
-                this.VarA5.Text = "a";
-                this.VarB5.Text = "b";
-                this.VarC5.Text = "c";
-                this.VarA6.Text = "a";
-                this.VarB6.Text = "b";
-                this.Nomb1.Text = "Nombre";
-                this.Nomb3.Text = "Nombre";
-                this.Nomb4.Text = "Nombre";
-                this.Nomb5.Text = "Nombre";
-                this.Nomb6.Text = "Nombre";
-
+                funcionSeleccionada = 2;
+                OcultaFunc1();
+                OcultaFunc3();
+                OcultaFunc4();
+                OcultaFunc5();
+                OcultaFunc6();
             }
             else
             {
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
+                OcultaFunc2();
             }
         }
 
@@ -131,46 +75,16 @@ namespace Trabajo
                 VarA3.Visibility = Visibility.Visible;
                 VarB3.Visibility = Visibility.Visible;
                 Nomb3.Visibility = Visibility.Visible;
-
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
-                this.VarA1.Text = "a";
-                this.VarB1.Text = "b";
-                this.VarA2.Text = "a";
-                this.VarB2.Text = "b";
-                this.VarA4.Text = "a";
-                this.VarB4.Text = "b";
-                this.VarA5.Text = "a";
-                this.VarB5.Text = "b";
-                this.VarC5.Text = "c";
-                this.VarA6.Text = "a";
-                this.VarB6.Text = "b";
-                this.Nomb1.Text = "Nombre";
-                this.Nomb2.Text = "Nombre";
-                this.Nomb4.Text = "Nombre";
-                this.Nomb5.Text = "Nombre";
-                this.Nomb6.Text = "Nombre";
-
+                funcionSeleccionada = 3;
+                OcultaFunc1();
+                OcultaFunc2();
+                OcultaFunc4();
+                OcultaFunc5();
+                OcultaFunc6();
             }
             else
             {
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
+                OcultaFunc3();
             }
         }
 
@@ -181,46 +95,16 @@ namespace Trabajo
                 VarA4.Visibility = Visibility.Visible;
                 VarB4.Visibility = Visibility.Visible;
                 Nomb4.Visibility = Visibility.Visible;
-
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
-                this.VarA1.Text = "a";
-                this.VarB1.Text = "b";
-                this.VarA2.Text = "a";
-                this.VarB2.Text = "b";
-                this.VarA3.Text = "a";
-                this.VarB3.Text = "b";
-                this.VarA5.Text = "a";
-                this.VarB5.Text = "b";
-                this.VarC5.Text = "c";
-                this.VarA6.Text = "a";
-                this.VarB6.Text = "b";
-                this.Nomb1.Text = "Nombre";
-                this.Nomb2.Text = "Nombre";
-                this.Nomb3.Text = "Nombre";
-                this.Nomb5.Text = "Nombre";
-                this.Nomb6.Text = "Nombre";
-
+                funcionSeleccionada = 4;
+                OcultaFunc1();
+                OcultaFunc2();
+                OcultaFunc3();
+                OcultaFunc5();
+                OcultaFunc6();
             }
             else
             {
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
+                OcultaFunc4();
             }
         }
 
@@ -232,46 +116,16 @@ namespace Trabajo
                 VarB5.Visibility = Visibility.Visible;
                 VarC5.Visibility = Visibility.Visible;
                 Nomb5.Visibility = Visibility.Visible;
-
-
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
-                this.VarA1.Text = "a";
-                this.VarB1.Text = "b";
-                this.VarA2.Text = "a";
-                this.VarB2.Text = "b";
-                this.VarA3.Text = "a";
-                this.VarB3.Text = "b";
-                this.VarA4.Text = "a";
-                this.VarB4.Text = "b";
-                this.VarA6.Text = "a";
-                this.VarB6.Text = "b";
-                this.Nomb1.Text = "Nombre";
-                this.Nomb2.Text = "Nombre";
-                this.Nomb3.Text = "Nombre";
-                this.Nomb4.Text = "Nombre";
-                this.Nomb6.Text = "Nombre";
-
+                funcionSeleccionada = 5;
+                OcultaFunc1();
+                OcultaFunc2();
+                OcultaFunc3();
+                OcultaFunc4();
+                OcultaFunc6();
             }
             else
             {
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
+                OcultaFunc5();
             }
         }
 
@@ -282,48 +136,64 @@ namespace Trabajo
                 VarA6.Visibility = Visibility.Visible;
                 VarB6.Visibility = Visibility.Visible;
                 Nomb6.Visibility = Visibility.Visible;
-
-                VarA1.Visibility = Visibility.Hidden;
-                VarB1.Visibility = Visibility.Hidden;
-                Nomb1.Visibility = Visibility.Hidden;
-                VarA2.Visibility = Visibility.Hidden;
-                VarB2.Visibility = Visibility.Hidden;
-                Nomb2.Visibility = Visibility.Hidden;
-                VarA3.Visibility = Visibility.Hidden;
-                VarB3.Visibility = Visibility.Hidden;
-                Nomb3.Visibility = Visibility.Hidden;
-                VarA4.Visibility = Visibility.Hidden;
-                VarB4.Visibility = Visibility.Hidden;
-                Nomb4.Visibility = Visibility.Hidden;
-                VarA5.Visibility = Visibility.Hidden;
-                VarB5.Visibility = Visibility.Hidden;
-                VarC5.Visibility = Visibility.Hidden;
-                Nomb5.Visibility = Visibility.Hidden;
-                this.VarA1.Text = "a";
-                this.VarB1.Text = "b";
-                this.VarA2.Text = "a";
-                this.VarB2.Text = "b";
-                this.VarA3.Text = "a";
-                this.VarB3.Text = "b";
-                this.VarA4.Text = "a";
-                this.VarB4.Text = "b";
-                this.VarA5.Text = "a";
-                this.VarB5.Text = "b";
-                this.VarC5.Text = "c";
-                this.Nomb1.Text = "Nombre";
-                this.Nomb2.Text = "Nombre";
-                this.Nomb3.Text = "Nombre";
-                this.Nomb4.Text = "Nombre";
-                this.Nomb5.Text = "Nombre";
-
-
+                funcionSeleccionada = 6;
+                OcultaFunc1();
+                OcultaFunc2();
+                OcultaFunc3();
+                OcultaFunc4();
+                OcultaFunc5();
             }
             else
             {
-                VarA6.Visibility = Visibility.Hidden;
-                VarB6.Visibility = Visibility.Hidden;
-                Nomb6.Visibility = Visibility.Hidden;
+                OcultaFunc6();
             }
+        }
+
+        private void Incluye_Click(object sender, RoutedEventArgs e)
+        {
+
+            string verifica = "^[0-9]";
+
+            if (funcionSeleccionada == 0)
+                MessageBox.Show("Elige una expresion, pon los parametros y el color");
+            switch (funcionSeleccionada)
+            {
+                case 1:
+                    a = Regex.IsMatch(VarA1.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB1.Text.ToString(), verifica);
+                    nomb_func =string.IsNullOrEmpty(Nomb1.Text);
+                    break;
+                case 2:
+                    a = Regex.IsMatch(VarA2.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB2.Text.ToString(), verifica);
+                    nomb_func = string.IsNullOrEmpty(Nomb2.Text);
+                    break;
+                case 3:
+                    a = Regex.IsMatch(VarA3.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB3.Text.ToString(), verifica);
+                    nomb_func = string.IsNullOrEmpty(Nomb3.Text);
+                    break;
+                case 4:
+                    a = Regex.IsMatch(VarA4.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB4.Text.ToString(), verifica);
+                    nomb_func = string.IsNullOrEmpty(Nomb4.Text);
+                    break;
+                case 5:
+                    a = Regex.IsMatch(VarA5.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB5.Text.ToString(), verifica);
+                    c = Regex.IsMatch(VarC5.Text.ToString(), verifica);
+                    nomb_func = string.IsNullOrEmpty(Nomb5.Text);
+                    break;
+                case 6:
+                    a = Regex.IsMatch(VarA6.Text.ToString(), verifica);
+                    b = Regex.IsMatch(VarB6.Text.ToString(), verifica);
+                    nomb_func = string.IsNullOrEmpty(Nomb6.Text);
+                    break;
+            }
+            if (!a || !b || ((funcionSeleccionada == 5) && !c))
+                MessageBox.Show("Falta introducir un parametro");
+            if(nomb_func) // la funcion isNullOfEmpty devuelve verdadero en caso de que este vacia
+                MessageBox.Show("Falta introducir el nombre");
         }
 
         private void VarA1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -420,10 +290,64 @@ namespace Trabajo
         {
             Nomb6.Text = "";
         }
-
-        private void Incluye_Click(object sender, RoutedEventArgs e)
+        
+        private void OcultaFunc1()
         {
-
+            this.VarA1.Text = "a";
+            this.VarB1.Text = "b";
+            this.Nomb1.Text = "Nombre";
+            VarA1.Visibility = Visibility.Hidden;
+            VarB1.Visibility = Visibility.Hidden;
+            Nomb1.Visibility = Visibility.Hidden;
         }
+        private void OcultaFunc2()
+        {
+            this.VarB1.Text = "b";
+            this.VarA2.Text = "a";
+            this.Nomb2.Text = "Nombre";
+            VarA2.Visibility = Visibility.Hidden;
+            VarB2.Visibility = Visibility.Hidden;
+            Nomb2.Visibility = Visibility.Hidden;
+        }
+        private void OcultaFunc3()
+        {
+            this.VarA3.Text = "a";
+            this.VarB3.Text = "b";
+            this.Nomb3.Text = "Nombre";
+            VarA3.Visibility = Visibility.Hidden;
+            VarB3.Visibility = Visibility.Hidden;
+            Nomb3.Visibility = Visibility.Hidden;
+        }
+        
+        private void OcultaFunc4()
+        {
+            this.VarA4.Text = "a";
+            this.VarB4.Text = "b";
+            this.Nomb4.Text = "Nombre";
+            VarA4.Visibility = Visibility.Hidden;
+            VarB4.Visibility = Visibility.Hidden;
+            Nomb4.Visibility = Visibility.Hidden;
+        }
+        private void OcultaFunc5()
+        {
+            this.VarA5.Text = "a";
+            this.VarB5.Text = "b";
+            this.VarC5.Text = "c";
+            this.Nomb5.Text = "Nombre";
+            VarA5.Visibility = Visibility.Hidden;
+            VarB5.Visibility = Visibility.Hidden;
+            VarC5.Visibility = Visibility.Hidden;
+            Nomb5.Visibility = Visibility.Hidden;
+        }
+        private void OcultaFunc6()
+        {
+            this.VarA6.Text = "a";
+            this.VarB6.Text = "b";
+            this.Nomb6.Text = "Nombre";
+            VarA6.Visibility = Visibility.Hidden;
+            VarB6.Visibility = Visibility.Hidden;
+            Nomb6.Visibility = Visibility.Hidden;
+        }
+        
     }
 }
